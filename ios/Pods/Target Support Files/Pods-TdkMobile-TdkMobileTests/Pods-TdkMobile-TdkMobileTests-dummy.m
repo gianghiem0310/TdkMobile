8a36dfb6d10a011c73911168225b610a494c534d
gianghiem0310 <jangiim93nt@gmail.com>
2024-07-09 16:35:27 +0700

    Update source Project

---
+++ ios/Pods/Target Support Files/Pods-TdkMobile-TdkMobileTests/Pods-TdkMobile-TdkMobileTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TdkMobile_TdkMobileTests : NSObject
+@end
+@implementation PodsDummy_Pods_TdkMobile_TdkMobileTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-TdkMobile/Pods-TdkMobile-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TdkMobile : NSObject
+@end
+@implementation PodsDummy_Pods_TdkMobile
+@end
